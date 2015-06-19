79e6d3fbe679c809bfec3cf92ccd89912ef3e2dd
guidowb <guidowb@gmail.com>
2015-06-19 07:54:28 -0500

    Added some words

--- config-client/get-config.sh
+++ config-client/get-config.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# The get-config.py program will directly manipulate things like
+# configuration files. But it can not directly manipulate the
+# environment variables, as that would only affect its process
+# hierarchy. So for environment variables, we have it provide
+# us with the keys and values on its output stream, and we place
+# them in exported environment variables, here - in the parent
+# process - which will also be the parent for the application.
+
 while read key value
 do
 	export "$key"="$value"

97e18aa8acd4d7cb8f4bcd0906a56169991d19c2
guidowb <guidowb@gmail.com>
2015-06-26 08:07:57 -0500

    JSON pretty print ignores non-JSON

--- config-demo/pp.py
+++ config-demo/pp.py
@@ -1,5 +1,11 @@
 import sys
 import json
 
-doc = json.load(sys.stdin)
-json.dump(doc, sys.stderr, indent=4)
+input = sys.stdin.readlines()
+try:
+	doc = json.loads(''.join(input))
+	json.dump(doc, sys.stderr, indent=4)
+	print
+except:
+	for line in input:
+		print line

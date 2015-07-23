66854574c43f83c598edb683057fa40b9df39fd1
guidowb <guidowb@gmail.com>
2015-07-23 14:02:28 -0500

    Cleanup

--- config-demo/cups.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cf delete-service -f spring-config-server
-cf delete-service -f archaius-config-server
-cf delete-service -f zuul-config-server
-
-cf cups spring-config-server -p '{"url":"http://spring-cloud-config-server.cfapps.io","tags":["spring-config-server"]}'
-cf cups archaius-config-server -p '{"url":"http://archaius-config-server.cfapps.io","tags":["archaius-config-server"]}'
-cf cups zuul-config-server -p '{"url":"http://zuul-config-server.cfapps.io","tags":["zuul-config-server"]}'
